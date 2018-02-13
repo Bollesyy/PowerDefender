637aa6d01f7ae7e230905c43e969299a346f7a74
Bollesyy <33631907+Bollesyy@users.noreply.github.com>
2018-02-13 16:32:43 -0500

    Add files via upload

---
+++ Hallway2TargetEnable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Hallway2TargetEnable : MonoBehaviour {
+
+	private int target;
+	public static Collider collider2;
+	void Start () {
+		collider2 = GetComponent<BoxCollider> ();
+		target = FirstPersonController.TargetsCurrent;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		target = FirstPersonController.TargetsCurrent;
+		if (target == 1 ) {
+			collider2.enabled = true;
+			Debug.Log ("Hallway2 Engaged");
+		} else if (target != 1) {
+			collider2.enabled = false;
+			Debug.Log ("Disengaged Hallway2");
+		}
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.name.Contains ("Enemy")) {
+			if (collider2.enabled) {
+				col.gameObject.tag = "EnemyTargets";
+			}
+		}
+	}
+
+	void OnTriggerExit (Collider col)
+	{
+		for (int i = 0; i < AutoLock.enemyObjects.Length; i++) {
+			AutoLock.enemyObjects [i] = null;
+		}
+
+
+	}
+}
---
+++ HallwayTargetEnable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class HallwayTargetEnable : MonoBehaviour {
+	
+	private int target;
+	public static Collider collider1;
+	void Start () {
+		collider1 = GetComponent<BoxCollider> ();
+		target = FirstPersonController.TargetsCurrent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = FirstPersonController.TargetsCurrent;
+		if (target == 0 ) {
+			collider1.enabled = true;
+			Debug.Log ("Hallway1 Engaged");
+		} else if (target != 0) {
+			collider1.enabled = false;
+		}
+			Debug.Log ("Disengaged Hallway1");
+		}
+
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.name.Contains("Enemy")) {
+			if (collider1.enabled) {
+				col.gameObject.tag = "EnemyTargets";
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		for (int i = 0; i < AutoLock.enemyObjects.Length; i++) {
+			AutoLock.enemyObjects [i] = null;
+		}
+	}
+}
+
+
